4864d02991ccc8288df2db87eecae6b0b7bc4054
Carlos Pieper <capieper.pieper@gmail.com>
2024-10-01 22:02:41 -0300

    update

--- TCC/TCC.ino
+++ TCC/TCC.ino
@@ -1,7 +1,20 @@
 #include <SoftwareSerial.h>
-#include <WiFi.h>
+#include <WiFiEsp.h>
 
-SoftwareSerial GPSModule(4, 3);  // RX, TX
+SoftwareSerial GPSModule(4, 3);
+SoftwareSerial wifiModule(7,6);
+#define echoSensor 8
+#define trigSensor 9
+
+const char rede[] = "ap-102";
+const char senha[] = "cincopila";
+
+bool wifiConectado = false;
+
+WiFiEspServer server(80);
+ 
+RingBuffer buf(8)
+
 int updates;
 int failedUpdates;
 int pos;
@@ -11,38 +24,36 @@ String timeUp;
 String nmea[15];
 float latitude = 0, longitude = 0;
 
-void setup() {
-  Serial.begin(57600);
-  GPSModule.begin(9600);
-  char* ssid = "ap102";
-  char* password = "cincopila";
-  
-  WiFi.begin(ssid, password);
+bool vagaOcupada() {
+  bool ocupada = false;
 
-  while (WiFi.status() != WL_CONNECTED) {
-    Serial.println("tentando conectar");
+  digitalWrite(trigSensor, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigSensor, LOW);
+
+  double distancia = pulseIn(echoSensor, HIGH);
+  Serial.print("distancia pega ");
+  Serial.print(distancia);
+
+  distancia *=340;
+  distancia /= 2;
+  distancia /= 10000; 
+
+  Serial.print("distancia: ");
+  Serial.print(distancia);
+
+  if(distancia < 100){
+    ocupada = true;
   }
 
-  
-    Serial.println(WiFi.localIP());
+  return ocupada;
 }
 
-void loop() {
-  Serial.println("---------------------");
-  Serial.println("Latitude");
-  Serial.println(latitude);
-  Serial.println("Longitude");
-  Serial.println(longitude);
-
-  UpdateLocation();
-
-  delay(1000);
-}
-
-void UpdateLocation() {
+void updateLocation(){
   Serial.flush();
   GPSModule.flush();
-  while (GPSModule.available() > 0) {
+  while (GPSModule.available() > 0)
+  {
     GPSModule.read();
   }
   if (GPSModule.find("$GPRMC,")) {
@@ -62,7 +73,8 @@ void UpdateLocation() {
     nmea[4] = ConvertLng();
     latitude = nmea[2].toDouble();
     longitude = nmea[4].toDouble();
-  } else {
+  }
+  else {
     failedUpdates++;
   }
   stringplace = 0;
@@ -87,7 +99,8 @@ String ConvertLat() {
 
   char charVal[9];
   dtostrf(latsecond, 4, 6, charVal);
-  for (int i = 0; i < sizeof(charVal); i++) {
+  for (int i = 0; i < sizeof(charVal); i++)
+  {
     CalcLat += charVal[i];
   }
   latfirst += CalcLat.substring(1);
@@ -109,16 +122,64 @@ String ConvertLng() {
       //Serial.println(lngfirst);
       lngsecond = nmea[4].substring(i - 2).toFloat();
       //Serial.println(lngsecond);
+
     }
   }
   lngsecond = lngsecond / 60;
   String CalcLng = "";
   char charVal[9];
   dtostrf(lngsecond, 4, 6, charVal);
-  for (int i = 0; i < sizeof(charVal); i++) {
+  for (int i = 0; i < sizeof(charVal); i++)
+  {
     CalcLng += charVal[i];
   }
   lngfirst += CalcLng.substring(1);
   lngfirst = posneg += lngfirst;
   return lngfirst;
+}
+
+
+void setup() {
+  Serial.begin(57600);
+  GPSModule.begin(9600);
+  wifiModule.begin(115200);
+  WiFi.init(&wifiModule);
+  WiFi.config(IPAddress(192,168,0,110));
+
+  if(WiFi.status() == WL_NO_SHIELD){
+    while (true);
+  }
+  while(status != WL_CONNECTED){
+    status = WiFi.begin(ssid, pass);
+  }
+  server.begin();
+
+  pinMode(trigSensor, OUTPUT);
+  pinMode(echoSensor, INPUT);
+}
+
+void loop() {
+  WiFiEspClient client = server.available();
+  
+  Serial.println("---------------------");
+  Serial.println("Latitude");
+  Serial.println(latitude);
+  Serial.println("Longitude");
+  Serial.println(longitude);
+  
+  bool ocupada = vagaOcupada();
+
+  if(ocupada){
+    updateLocation();
+  }
+  
+  if(wifiModule.available())       /* Confere se a comunicação está acessível */
+  {
+    while(wifiModule.available()) {  /* Enquanto estiver acessível */
+      char c = wifiModule.read();      /* Le o caractere. */
+      Serial.write(c);              /* Escreve no monitor serial */
+    }
+  }
+
+  delay(1000);
 }
